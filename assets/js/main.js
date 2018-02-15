75c6d2614b3121095c211365a843d88017e1a889
claireisabelle <claire.bourdale@gmail.com>
2018-02-15 12:13:06 +0100

    Structure HTML et CSS

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+/*
+ *********************************
+	AJOUTER UNE TACHE
+ *********************************
+ */
+
+document.getElementById('add').addEventListener('click', function(){
+	
+	var value = document.getElementById('item').value;
+
+	if(value){
+		
+	}
+
+});
